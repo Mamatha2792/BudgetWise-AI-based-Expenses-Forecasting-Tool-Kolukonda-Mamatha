ed3d79ff2021f4d019c94c9f8db0c1e4a6f6d64f
Mamatha2792 <vy863170@gmail.com>
2025-10-17 13:33:24 +0530

    Add function to train a simple linear regression model

--- src/src/model.py
+++ src/src/model.py
@@ -1,1 +1,9 @@
 
+from sklearn.linear_model import LinearRegression
+
+def train_simple_model(df):
+    X = df[['month_index']].values
+    y = df['expense'].values
+    model = LinearRegression()
+    model.fit(X, y)
+    return model
